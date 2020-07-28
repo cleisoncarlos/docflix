03786a08456f19831c0cc28e628d3b2d1e68a6d5
Cleison Carlos <cleisoncarlos@hotmail.com>
2020-07-28 17:03:52 -0300

    Primeiro dia da Imersao alura

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import dadosIniciais from './data/dados_iniciais.json';
+import Menu from './components/Menu'
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ background: "#141414" }}>
+      <Menu />
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"O filme narra a história do jovem Aaron Swartz (1986-2013), um jovem programador norte-americano que acreditava na mudança radical do mundo através da internet e da computação. Durante toda a sua vida, Aaron usou a programação computacional como uma forma de nos ajudar a resolver problemas e tornar o mundo um lugar mais democrático, justo e eficiente. Em uma destas tentativas, Aaron irá usar a rede do MIT (Massachusetts Institute of Technology) para realizar o download massivo de milhões de artigos acadêmicos de uma base de dados privada chamada JSTOR. Nesse meio-tempo, o Ministério Público dos Estados Unidos irá conduzir um processo criminal contra Aaron, que termina por levá-lo ao suicídio. "}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[1]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[2]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[3]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[4]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[5]}
+      />      
+
+      <Footer />
     </div>
   );
 }
